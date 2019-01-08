ce0d5cf4439eaad72ea02858d2496b219ea145a5
Unknown <sucre.91@hotmail.it>
2019-01-08 15:59:31 +0100

    Enabling path autocompletition

--- MT-Doctor.sh
+++ MT-Doctor.sh
@@ -4,7 +4,7 @@ echo "I am MT-Doctor, the TimeMachine® Doctor (hopefully you get the reference 
 echo "This script tries to recover an Apple® TimeMachine sparsebundle corrupted by (malicious?) networking or power glitches."
 echo -e "\nIMPORTANT: for security reasons this script assumes the volume where the sparsebundle file is located is already connected to this machine (wheter it is an external hard disk or a NAS)"
 
-read -p "** Enter TimeMachine sparsebundle path: " $SBFullPath
+read -e -p "** Enter TimeMachine sparsebundle path: " $SBFullPath
 
 if (whoami = root)
     echo -e "\n** Stopping TimeMachine® service..."
